c53d5fec9eaf06ef8309bff93f023480001fbe93
Matheus Augusto <matheus_adps@hotmail.com>
2022-03-12 09:50:00 -0300

    First Commit

---
+++ src/test/java/com/neptum/BankAccountApi/BankAccountApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neptum.BankAccountApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
